bcd444a493b591cc26c5c931f268e311d31308f2
mBentaifour <bentaifourmoh@gmail.com>
2024-10-11 18:27:08 -0700

    Variable Annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''task 2's module
+'''
+
+
+import math
+
+
+def floor(n: float) -> int:
+    '''computes the floor of a floating-point number
+    '''
+    return math.floor(n)
